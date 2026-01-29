ff50bbc9e77e78081c929b6da7b57f64333cfe40
fabian-berencreutz <fabianberencreutz@gmail.com>
2026-01-29 12:44:35 +0100

    bugfix och meny

--- src/main/java/se/iths/fabian/gitlabb1/controller/ProductController.java
+++ src/main/java/se/iths/fabian/gitlabb1/controller/ProductController.java
@@ -1,15 +1,20 @@
 package se.iths.fabian.gitlabb1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
     @GetMapping
-    public String products() {
+    public String products(Model model) {
+        List<String> products = List.of("Laptop", "Phone", "Tablet");
+        model.addAttribute("products", products);
         return "products";
     }
 }
