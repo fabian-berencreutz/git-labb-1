de40b13bea2af9328a12fd607a4822bce2a95c32
anastevanovic <Ana.Stevanovic@iths.se>
2026-01-29 12:23:41 +0100

    Add @RequestMapping to controllers

--- src/main/java/se/iths/fabian/gitlabb1/controller/AboutController.java
+++ src/main/java/se/iths/fabian/gitlabb1/controller/AboutController.java
@@ -2,11 +2,13 @@ package se.iths.fabian.gitlabb1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/about")
 public class AboutController {
 
-    @GetMapping("/about")
+    @GetMapping
     public String about() {
         return "about";
     }
--- src/main/java/se/iths/fabian/gitlabb1/controller/ProductController.java
+++ src/main/java/se/iths/fabian/gitlabb1/controller/ProductController.java
@@ -1,18 +1,15 @@
 package se.iths.fabian.gitlabb1.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/products")
-    public String products(Model model) {
-        List<String> products = List.of("Laptop", "Phone", "Tablet");
-        model.addAttribute("products", products);
+    @GetMapping
+    public String products() {
         return "products";
     }
 }
