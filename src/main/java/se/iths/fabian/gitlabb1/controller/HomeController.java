d9cd588f9dab14f50f9a5ad61c5cdd6630119c6e
anastevanovic <Ana.Stevanovic@iths.se>
2026-01-29 08:51:53 +0100

    Change greeting in HomeController (A)

--- src/main/java/se/iths/fabian/gitlabb1/controller/HomeController.java
+++ src/main/java/se/iths/fabian/gitlabb1/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("greeting", "Välkommen!");
+        model.addAttribute("greeting", "Välkommen från A!");
         return "home";
     }
 }
