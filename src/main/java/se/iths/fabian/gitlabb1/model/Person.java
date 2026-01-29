40b10e1f084cc03d650e642753176c791dbd0288
anastevanovic <Ana.Stevanovic@iths.se>
2026-01-29 13:15:03 +0100

    Add attributes to Person model

--- src/main/java/se/iths/fabian/gitlabb1/model/Person.java
+++ src/main/java/se/iths/fabian/gitlabb1/model/Person.java
@@ -1,20 +1,26 @@
 package se.iths.fabian.gitlabb1.model;
 
 public class Person {
+
     private String name;
+    private int age;
+    private String email;
 
-    public Person() {
-    }
-
-    public Person(String name) {
+    public Person(String name, int age, String email) {
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
